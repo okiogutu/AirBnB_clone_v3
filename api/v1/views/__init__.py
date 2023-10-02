578d9b3745c464225662405adc977cd87325b87e
wosohd <wosohd@gmail.com>
2023-10-02 14:38:24 -0700

    -minit update

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Int file that Contains 
-the blueprint for the API"""
+"""API blueprint"""
 from flask import Blueprint
 
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+'''The blueprint for the AirBnB clone API.'''
 
 
 from api.v1.views.amenities import *
